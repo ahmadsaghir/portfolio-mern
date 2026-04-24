b3461a3dbcea8a623a70d97566b31ff401ee6289
Ahmad Saghir <ahmad.saghir.1999@gmail.com>
2026-04-24 12:19:10 +0200

    fix: add critical inline styles for fast initial render and improve CSS loading

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,5 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Plugin to defer CSS loading and reduce render-blocking
+    {
+      name: 'defer-css',
+      apply: 'build',
+      transformIndexHtml(html) {
+        // Replace render-blocking stylesheet links with deferred versions
+        return html.replace(
+          /<link([^>]*?)rel="stylesheet"([^>]*?)>/g,
+          '<link$1rel="stylesheet" media="print" onload="this.media=\'all\'"$2><noscript><link$1rel="stylesheet"$2></noscript>'
+        );
+      }
+    }
+  ]
 });
